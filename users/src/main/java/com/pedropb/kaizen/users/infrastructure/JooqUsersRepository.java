cd7198834a0266de33318c646ff03ba354361611
Pedro Baracho <pedro.baracho@skipthedishes.ca>
2019-04-12 16:41:55 -0500

    Simplify dependency injection

--- users/src/main/java/com/pedropb/kaizen/users/infrastructure/JooqUsersRepository.java
+++ users/src/main/java/com/pedropb/kaizen/users/infrastructure/JooqUsersRepository.java
@@ -1,14 +1,22 @@
 package com.pedropb.kaizen.users.infrastructure;
 
+import com.google.inject.Inject;
 import com.pedropb.kaizen.users.domain.User;
 import com.pedropb.kaizen.users.domain.UserQuery;
 import com.pedropb.kaizen.users.domain.UsersRepository;
 
+import javax.sql.DataSource;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class JooqUsersRepository implements UsersRepository {
+
+    @Inject
+    public JooqUsersRepository() {
+
+    }
+
     @Override
     public List<User> findUsers(UserQuery query) {
         return Collections.emptyList();
--- users/src/main/java/com/pedropb/kaizen/users/infrastructure/UsersModule.java
+++ users/src/main/java/com/pedropb/kaizen/users/infrastructure/UsersModule.java
@@ -6,22 +6,13 @@ import com.pedropb.kaizen.users.domain.UsersService;
 
 public class UsersModule extends AbstractModule {
 
-    private final UsersService usersService;
-    private final UsersResources usersResources;
-    private final JooqUsersRepository jooqUsersRepository;
-
-    public UsersModule() {
-        this.jooqUsersRepository = new JooqUsersRepository();
-        this.usersService = new UsersService(jooqUsersRepository);
-        this.usersResources = new UsersResources(usersService);
-    }
-
     @Override
     protected void configure() {
-        bind(JooqUsersRepository.class).toInstance(jooqUsersRepository);
-        bind(UsersService.class).toInstance(usersService);
-        bind(UsersResources.class).toInstance(usersResources);
+        bind(JooqUsersRepository.class);
+        bind(UsersService.class);
+        bind(UsersResources.class);
 
         bind(UsersRepository.class).to(JooqUsersRepository.class);
+//        bind(DataSource.class).toInstance();
     }
 }
